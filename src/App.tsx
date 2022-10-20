44af7c89ecb2e4aa21c5219fb83164d9d85a1ba9
ChristofferBillman <christoffer.billman@gmail.com>
2022-10-20 19:43:25 +0200

    feat: Map and RightMenu components added + added styling for text

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './styles/App.css'
+import './styles/Type.css'
 
 import Menu from './components/Menu'
 import Map from './components/Map'
+import RightMenu from './components/RightMenu'
+
+import { useState } from 'react'
 
 export default function App(): JSX.Element {
 
+	const [displayColored, setDisplayColored] = useState<boolean>(false)
 	return (
 		<div className="App">
 			<Menu/>
-			<Map/>
+			<Map displayColored={displayColored}/>
+			<RightMenu displayColored={displayColored} setDisplayColored={setDisplayColored}/>
 		</div>
 	)
 }
